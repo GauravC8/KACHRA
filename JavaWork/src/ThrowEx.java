f55808b06d0522cf0840c9dea1e603175736433a
GauravC8 <mail4gauravchauhan@gmail.com>
2019-02-15 11:31:18 +0530

    throw

--- JavaWork/src/ThrowEx.java
+++ JavaWork/src/ThrowEx.java
@@ -4,7 +4,9 @@ public class ThrowEx extends Exception
 	private static int accNo[] = {1001,1002,1003,1004};
 	private static String name[]= {"A","B","C","D","E"};
 	private static double bal[]= {10000.00,12000.00,5600.0,999.0,1100.55};
+	
 	ThrowEx(String str){super(str);}
+	
 	public static void main(String[] args) {
 		try {
 			System.out.println("Account No:" + "\t"+"CUSTOMER"+"\t"+"BALANCE");
