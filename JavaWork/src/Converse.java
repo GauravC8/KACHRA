eba83d375f7178fe72028d16c30d264050ff1c86
GauravC8 <mail4gauravchauhan@gmail.com>
2019-02-15 16:17:52 +0530

    commmited

--- JavaWork/src/Converse.java
+++ JavaWork/src/Converse.java
@@ -1,11 +1,12 @@
 public class Converse {
-	void tobeConvert(final String[] args) {
+	void tobeConvert(final String[] args)
+	{
 		String binary, hex, octal;
 		int num = 100;
 		Integer obj = new Integer(num);
-		binary = obj.toBinaryString(num);
-		octal = obj.toOctalString(num);
-		hex = obj.toHexString(num);
+		binary = Integer.toBinaryString(num);
+		octal = Integer.toOctalString(num);
+		hex = Integer.toHexString(num);
 
 		System.out.println("Decimal Value :" + num);
 		System.out.println("Binary" + binary);
@@ -13,10 +14,9 @@ public class Converse {
 		System.out.println("Octal " + octal);
 
 	}
-
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		Converse c = new Converse();
 		c.tobeConvert(args);
 	}
-
 }
--- JavaWork/src/Wrapper.java
+++ JavaWork/src/Wrapper.java
@@ -14,9 +14,11 @@ public class Wrapper {
 		Integer intObj1 = new Integer (25);
 		Integer intObj2 = new Integer ("25");
 		Integer intObj3= new Integer (35);
+		
 		//compareTo demo
 		System.out.println("Comparing using compareTo Obj1 and Obj2: " + intObj1.compareTo(intObj2));
 		System.out.println("Comparing using compareTo Obj1 and Obj3: " + intObj1.compareTo(intObj3));
+		
 		//Equals demo
 		System.out.println("Comparing using equals Obj1 and Obj2: " + intObj1.equals(intObj2));
 		System.out.println("Comparing using equals Obj1 and Obj3: " + intObj1.equals(intObj3));
@@ -25,8 +27,9 @@ public class Wrapper {
 		Float f3 = new Float(2.25f);
 		System.out.println("Comparing using compare f1 and f2: " +Float.compare(f1,f2));
 		System.out.println("Comparing using compare f1 and f3: " +Float.compare(f1,f3));
+		
 		//Addition of Integer with Float
 		Float f = intObj1.floatValue() + f1;
-		System.out.println("Addition of intObj1 and f1: "+ intObj1 +"+" +f1+"=" +f );
+		System.out.println("Addition of intObj1 and f1: "+ intObj1 +"+" +f1+"=" + f );
 	}
 }
