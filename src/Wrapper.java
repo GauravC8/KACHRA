aad25fad7adaf6905f36e6f1c7a7ea48319d53b7
GauravC8 <mail4gauravchauhan@gmail.com>
2019-02-16 11:17:24 +0530

    committed

---
+++ src/Wrapper.java
@@ -0,0 +1,35 @@
+public class Wrapper {
+
+	public static void main(String[] args) {
+		/*String number =args[0];
+		Byte byNum=Byte.valueOf(number);
+		Short shNum=Short.valueOf(number);
+		Integer num = Integer.valueOf(number);
+		Long lgNum=Long.valueOf(number);
+		System.out.println(byNum);
+		System.out.println(shNum);
+		System.out.println(num);
+		System.out.println(lgNum);
+		*/
+		Integer intObj1 = new Integer (25);
+		Integer intObj2 = new Integer ("25");
+		Integer intObj3= new Integer (35);
+		
+		//compareTo demo
+		System.out.println("Comparing using compareTo Obj1 and Obj2: " + intObj1.compareTo(intObj2));
+		System.out.println("Comparing using compareTo Obj1 and Obj3: " + intObj1.compareTo(intObj3));
+		
+		//Equals demo
+		System.out.println("Comparing using equals Obj1 and Obj2: " + intObj1.equals(intObj2));
+		System.out.println("Comparing using equals Obj1 and Obj3: " + intObj1.equals(intObj3));
+		Float f1 = new Float("2.25f");
+		Float f2 = new Float("20.43f");
+		Float f3 = new Float(2.25f);
+		System.out.println("Comparing using compare f1 and f2: " +Float.compare(f1,f2));
+		System.out.println("Comparing using compare f1 and f3: " +Float.compare(f1,f3));
+		
+		//Addition of Integer with Float
+		Float f = intObj1.floatValue() + f1;
+		System.out.println("Addition of intObj1 and f1: "+ intObj1 +"+" +f1+"=" + f );
+	}
+}
---
+++ src/oracle/Software1.java
@@ -0,0 +1,18 @@
+package oracle;
+
+public class Software1 {
+
+	public Software1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public final void printSoftware()
+	{
+		int i;
+		String [] arOracle = {"Oracle8i","Oracle9i","Oracle10G","Oracle12o"};
+		for(i = 0;i<3;i++)
+		{
+			System.out.println(arOracle[i]);
+		}
+	}
+}
