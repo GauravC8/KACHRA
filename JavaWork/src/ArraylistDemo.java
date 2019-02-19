41d29c55327cc7ac9d00109f2d90f118c05a56db
GauravC8 <mail4gauravchauhan@gmail.com>
2019-02-19 12:48:11 +0530

    commited

---
+++ JavaWork/src/ArraylistDemo.java
@@ -0,0 +1,65 @@
+import java.util
+
+.ArrayList;
+import java.util.List;
+
+class Playerlist {
+	ArrayList playerArray;
+	List sublistobj;
+	
+	public Playerlist() {
+		playerArray = new ArrayList<>();
+		sublistobj = new ArrayList<>();
+	}
+
+	void add() {
+		for (int i = 0; i < 5; i++) 
+		{
+			playerArray.add(new Integer(i));
+		}
+		playerArray.add("Martine");
+		playerArray.add("Serena");
+		playerArray.add("Venus");
+		playerArray.add("MArk");
+		System.out.println();
+	}
+	void search()
+	{
+		System.out.println();
+		System.out.println("************************************************");
+		System.out.println("Search for object return first and last position");
+		System.out.println("************************************************");
+		System.out.println();
+		System.out.println("First occurance of string"+" \"Serena\" is at position-"+ playerArray.indexOf("Serena"));
+		System.out.println("Last occurance of string"+" \"Serena\" is at position-"+ playerArray.lastIndexOf("Serena"));
+	}
+	void display()
+	{
+		System.out.println("Retrive object from arraylist");
+		System.out.println("************************************************");
+		for(int i=0;i<playerArray.size();i++)
+		{
+			System.out.print("\t"+ playerArray.get(i));
+		}
+		System.out.println();
+	}
+	void extract()
+	{
+		System.out.println("extracting the sublist");
+		System.out.println("************************************************");
+		sublistobj = playerArray.subList(5, playerArray.size());
+		System.out.println(sublistobj);
+		System.out.println();
+	}
+}
+public class ArraylistDemo {
+
+	public static <E> void main(String[] args) {
+		// TODO Auto-generated method stub
+		Playerlist pl = new Playerlist();
+		pl.add();
+		pl.search();
+		pl.display();
+		pl.extract();		
+	}
+}
---
+++ JavaWork/src/MyBasicArrayList.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class MyBasicArrayList {
+
+	public static void main(String[] args) {
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("JAVA");
+		al.add("C++");
+		al.add("Python");
+		al.add("PHP");
+		System.out.println(al);
+		System.out.println("Element at Index 1..."+al.get(1));
+		System.out.println("Does list contains JAVA... "+al.contains("JAVA"));
+		al.add(2, "C");
+		System.out.println("\n"+al);
+		System.out.println("IS Array Empty...."+al.isEmpty());
+		System.out.println("Index of PHP is..."+al.indexOf("PHP"));
+		Collections.sort(al);
+		System.out.println("\n"+al);
+
+	}
+
+}
