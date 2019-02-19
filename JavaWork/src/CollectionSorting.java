2ee5b37f1d5e85cde568ad2c51df751354ae0234
GauravC8 <mail4gauravchauhan@gmail.com>
2019-02-19 16:05:08 +0530

    Commited

--- JavaWork/src/CollectionSorting.java
+++ JavaWork/src/CollectionSorting.java
@@ -9,16 +9,16 @@ public class CollectionSorting {
 		al.add("Dear");
 		al.add("Is");
 		al.add("Superb");
-
+//sort
 		Collections.sort(al);
-
 		System.out.println("List after the use of" + " Collection.sort() :\n" + al);
+//binarySearch
 		int pos = Collections.binarySearch(al, "Superb");
 		System.out.println("The position of \"Superb\" in Arraylist is :" + pos);
-
+//reverse
 		Collections.reverse(al);
 		System.out.println("List after the Reversing" + al);
-
+//swap
 		Collections.swap(al, 4, 1);
 		System.out.println("List after the SWaping" + al);
 
