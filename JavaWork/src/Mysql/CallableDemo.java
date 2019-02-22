c49cb3563f5bd67e5ce34ead11e1a94396801d74
GauravC8 <mail4gauravchauhan@gmail.com>
2019-02-22 11:37:05 +0530

    committed

--- JavaWork/src/Mysql/CallableDemo.java
+++ JavaWork/src/Mysql/CallableDemo.java
@@ -4,39 +4,39 @@ import java.sql.*;
 
 public class CallableDemo {
 
-		static Connection conn;
+	Connection conn;
+	ResultSet rs;
+
+	//creating the connection
+	void createConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/mysqljdbc", "root", "root");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	//getting dataset using procedure
+	public void getSkills(int candidateId) {
+		String query = "{call get_candidate_skill(?)}";  //Initializing the query for procedure
 		ResultSet rs;
-		CallableDemo()
-			{
-				
+		try {
+			CallableStatement smt = conn.prepareCall(query);  //calling the procedure
+			smt.setInt(1, candidateId);
+			rs = smt.executeQuery();
+			while (rs.next()) {
+				System.out.println(String.format("%s - %s",
+						rs.getString("first_name") + " " + rs.getString("last_name"), rs.getString("skill")));
 			}
-			void createConnection()
-			{
-				try {
-				Class.forName("com.mysql.jdbc.Driver");
-				conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/mysqljdbc", "root", "root");
-				}catch(Exception ex){
-				ex.printStackTrace();
-				}
-			}
-			public void getSkills(int candidateId)
-			{
-				String query = "{call get_candidate_skill(?)}";
-				ResultSet rs;
-				try
-				{
-					CallableStatement smt = conn.prepareCall(query);
-					smt.setInt(1, candidateId);
-					rs =smt.executeQuery();
-					while(rs.next())
-					{
-						System.out.println(String.format("%s - %s",rs.getString("first_name") + " "+ rs.getString("last_name"),rs.getString("skill")));
-					}
-				}catch(Exception ex) {ex.printStackTrace();}
-			}
-			public static void main(String[] args) {
-				CallableDemo cd =new CallableDemo();
-				cd.createConnection();
-				cd.getSkills(133);
-			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		CallableDemo cd = new CallableDemo();
+		cd.createConnection();
+		cd.getSkills(133);
+	}
 }
